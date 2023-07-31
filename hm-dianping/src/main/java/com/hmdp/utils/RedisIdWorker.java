36f3451bb9dc753bd647b4602d9d1d0a6b736495
panghuQvQ <1097155803@QQ.com>
2023-07-31 14:14:09 +0800

    init

--- hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -12,8 +12,9 @@ import java.time.format.DateTimeFormatter;
  * @title 基于 Redis的全局唯一ID生成器 工具类
  * @description 全局唯一ID(共64位)信息组成：
  *      1.符号位：0
- *      2.时间戳：31位 (当前时间 - 初始时间)
+ *      2.时间戳：31位 (当前时间戳 - 初始时间戳)
  *      3.序列号：32位
+ * 数据库存储的 id 为 long类型, 8个字节即 64位
  * @updateTime 2023/2/24 13:49
  * @throws
  */
@@ -43,7 +44,7 @@ public class RedisIdWorker {
         // 2.生成序列号
         // 2.1.获取当前日期，精确到天
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
-        // 2.2.自增长
+        // 2.2.自增长(占位: 32位)
         long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
 
         // 3.拼接并返回：时间戳向左移动32位   剩下的32位：或运算  自增长数据填充
@@ -57,7 +58,7 @@ public class RedisIdWorker {
     public static void main(String[] args) {
         // 指定获取2023年的起始时间
         LocalDateTime time = LocalDateTime.of(2023, 1, 1, 0, 0, 0);
-        // 获取秒数(时区)
+        // 获取秒数(时区偏移量) 1970-01-01T00:00:00Z
         long second = time.toEpochSecond(ZoneOffset.UTC);
         System.out.println("second=" + second);
     }
