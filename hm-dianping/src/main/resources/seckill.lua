0ad76f81034a0aa94bc7ab04aeda5a278966db8f
panghuQvQ <1097155803@QQ.com>
2023-08-01 14:28:55 +0800

    init

--- hm-dianping/src/main/resources/seckill.lua
+++ hm-dianping/src/main/resources/seckill.lua
@@ -30,14 +30,14 @@ if (tonumber(redis.call('get', stockKey)) <= 0) then
     return 1
 end
 
--- 3.2 判断用户是否下单 SISMEMBER orderKey userId
+-- 3.2 判断用户是否下单 SISMEMBER orderKey userId (Set类型,判断一个元素是否存在于set中)
 if (redis.call('sismember', orderKey, userId) == 1) then
     -- 3.2.1 存在，说明重复下单，返回2
     return 2
 end
 -- 3.3 扣库存 incrby stockKey -1
 redis.call('incrby', stockKey, -1)
--- 3.4 下单(保存用户) sadd orderKey userId
+-- 3.4 下单(保存用户) sadd seckill:order:xxx userId
 redis.call('sadd', orderKey, userId)
 -- 3.5 发送消息到队列中，XADD stream.order * k1 v1 k2 v2 k3 v3
 redis.call('xadd','stream.orders','*','userId',userId,'voucherId',voucherId,'id',orderId)
